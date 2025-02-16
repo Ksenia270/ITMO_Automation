64499208842359b335639b680c91153e8acd9773
Ксения <shutovaksenia27@mail.ru>
2025-02-16 21:09:35 +0300

    First Commit

---
+++ home_work/3_hw.py
@@ -0,0 +1,32 @@
+a=6
+b=5
+if a>=b: print(a)
+else: print(b)
+
+
+a=140
+b=5
+if (a-b)==135: print("YES")
+else: print("NO")
+
+a=12
+if a==1 or a==2 or a==12:
+    print("Зима")
+elif a in range(3,6):
+    print("Весна")
+elif a in range(6,9):
+    print("Лето")
+elif a in range(9,12):
+    print("Осень")
+
+
+a=62
+b=68
+c=64
+if a > 10 and b > 10 and c > 10:
+    print("yes")
+else:
+    print("no")
+
+
+
