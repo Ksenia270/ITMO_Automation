dd6a4e05e49552df421022b195c01667cce1e7c9
Ксения <shutovaksenia27@mail.ru>
2025-04-13 14:02:43 +0300

    Second Commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,36 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-driver=webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-text=driver.find_element(By.CSS_SELECTOR, '#user-name')
-if text is None:
-    print ('Элемент не найден')
-else:
-    print('Элемент найден')
-
-
-
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-text = driver.find_element(By.CSS_SELECTOR, '#password')
-if text is None:
-    print('Элемент не найден')
-else:
-     print('Элемент найден')
 
 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver=webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
 
-button=driver.find_element(By.CSS_SELECTOR, '#login-button')
-if button is None:
-    print ('Элемент не найден')
-else:
-    print('Элемент найден')
+def check_elements():
+    driver=webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    username_element=driver.find_element(By.CSS_SELECTOR, "#user-name")
+    password_element=driver.find_element(By.CSS_SELECTOR, "#password")
+    submit_button=driver.find_element(By.CSS_SELECTOR, "#login-button")
+
+    if username_element and password_element and submit_button:
+        print("Элементы найдены")
+
+    else:
+        print("Произошла ошибка")
+
+check_elements()
